0c19a99162f5d79fa49e4dedd8d771299cec3127
Noah Weiner <noahbweiner@gmail.com>
2020-09-22 11:10:09 -0700

    add interactible highlight

---
+++ Assets/Scripts/Interactible.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible : MonoBehaviour
+{
+    public string Action;
+    private bool isHighlighted;
+    public Material highlightMaterial;
+
+    private SpriteRenderer spriteRenderer;
+    private Material originalMaterial;
+
+    public bool IsHighlighted
+    {
+        get => isHighlighted;
+        set => UpdateIsHighlighted(value);
+    }
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            originalMaterial = spriteRenderer.material;
+        }
+    }
+
+    void UpdateIsHighlighted(bool value)
+    {
+        if (spriteRenderer != null && value != isHighlighted)
+        {
+            if (value)
+            {
+                spriteRenderer.material = highlightMaterial;
+            }
+            else
+            {
+                spriteRenderer.material = originalMaterial;
+            }
+
+            isHighlighted = value;
+        }
+    }
+}
---
+++ Assets/Scripts/InteractionController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour
+{
+    public Transform player;
+    public float interactionRadius = 3f;
+
+    private Interactible highighted;
+
+    void Update()
+    {
+        var items = GameObject.FindObjectsOfType<Interactible>().Where(item =>
+            (player.transform.position - item.transform.position).LessThan(interactionRadius));
+
+        if (!items.Any())
+        {
+            SetHighlighted(null);
+            return;
+        }
+
+        var closest = null as Interactible;
+        var closestDistSq = float.PositiveInfinity;
+        foreach (var item in items)
+        {
+            var distSq = (item.transform.position - player.transform.position).sqrMagnitude;
+
+            if (distSq < closestDistSq)
+            {
+                closest = item;
+                closestDistSq = distSq;
+            }
+        }
+
+        SetHighlighted(closest);
+    }
+
+    void SetHighlighted(Interactible value)
+    {
+        if (value != highighted)
+        {
+            if (highighted != null)
+            {
+                highighted.IsHighlighted = false;
+            }
+
+            if (value != null)
+            {
+                value.IsHighlighted = true;
+            }
+
+            highighted = value;
+        }
+    }
+}
